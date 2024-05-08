8e456ed0df4dbc4fe92fbd285a1206801094e4a9
riakgu <rizkyakbargusnaen@gmail.com>
2024-05-09 06:48:14 +0700

    Add useBodyInput hooks

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -3,11 +3,21 @@ import { useState } from 'react';
 function useInput(defaultValue = '') {
   const [value, setValue] = useState(defaultValue);
 
-  function handleValueChange({ target }) {
-    setValue(target.value);
-  }
+  const onValueChangeHandler = (event) => {
+    setValue(event.target.value);
+  };
 
-  return [value, handleValueChange, setValue];
+  return [value, onValueChangeHandler];
+}
+
+export function useBodyInput(defaultValue = '') {
+  const [value, setValue] = useState(defaultValue);
+
+  const onBodyChangeHandler = (event) => {
+    setValue(event.target.innerHTML);
+  };
+
+  return [value, onBodyChangeHandler];
 }
 
 export default useInput;
