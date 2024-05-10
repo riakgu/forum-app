58ba985b6c6a8146e94f46bba429abbd547efd76
riakgu <rizkyakbargusnaen@gmail.com>
2024-05-11 06:51:18 +0700

    Implements useRoutes and refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,38 @@
-import React, { useEffect } from 'react';
-import {Navigate, Route, Routes, useNavigate} from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { Toaster } from 'react-hot-toast';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import HomePage from './pages/HomePage';
-import ThreadPage from './pages/ThreadPage';
-import LeaderboardPage from './pages/LeaderboardPage';
-import ThreadAddPage from './pages/ThreadAddPage';
-import { asyncLogout } from './states/authUser/action';
-import { asyncIsPreload } from './states/isPreload/action';
-import Navbar from "./components/Navbar";
-import LoadingBar from 'react-redux-loading-bar';
+import React, { useEffect } from "react";
+import { Navigate, useNavigate, useRoutes } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import toast, { Toaster } from "react-hot-toast";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import HomePage from "./pages/HomePage";
+import ThreadPage from "./pages/ThreadPage";
+import LeaderboardPage from "./pages/LeaderboardPage";
+import ThreadAddPage from "./pages/ThreadAddPage";
+import { asyncLogout } from "./states/authUser/action";
+import { asyncIsPreload } from "./states/isPreload/action";
+import Navigation from "./components/Navigation";
+import LoadingBar from "react-redux-loading-bar";
+
+const ROUTE_PATHS = {
+  HOME: '/',
+  LOGIN: '/login',
+  REGISTER: '/register',
+  THREAD: '/thread/:id',
+  THREAD_CREATE: '/thread/create',
+  LEADERBOARD: '/leaderboard'
+};
 
 function App() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const authUser = useSelector(state => state.authUser);
-  const isPreload = useSelector(state => state.isPreload);
+  const authUser = useSelector((state) => state.authUser);
+  const isPreload = useSelector((state) => state.isPreload);
 
   const onLogout = () => {
     dispatch(asyncLogout()).then(({ status }) => {
-      if (status === 'success') navigate('/');
+      if (status === "success") navigate(ROUTE_PATHS.HOME);
+      toast.success("Logout successfully!")
     });
   };
 
@@ -30,29 +40,31 @@ function App() {
     dispatch(asyncIsPreload());
   }, []);
 
+  const routes = useRoutes([
+    { path: ROUTE_PATHS.HOME, element: <HomePage /> },
+    { path: ROUTE_PATHS.LOGIN, element: authUser ? <Navigate to={ROUTE_PATHS.HOME} /> : <LoginPage /> },
+    { path: ROUTE_PATHS.REGISTER, element: authUser ? <Navigate to={ROUTE_PATHS.HOME} /> : <RegisterPage /> },
+    { path: ROUTE_PATHS.THREAD, element: <ThreadPage /> },
+    { path: ROUTE_PATHS.THREAD_CREATE, element: <ThreadAddPage /> },
+    { path: ROUTE_PATHS.LEADERBOARD, element: <LeaderboardPage /> }
+  ]);
+
   if (isPreload) {
     return null;
   }
 
   return (
-    <div className="app-container">
-      <Toaster />
-      <header>
-        <h1>Forum App</h1>
-        <Navbar logout={onLogout} authUser={authUser} />
-      </header>
-      <LoadingBar style={{ backgroundColor: '#27374D' }}/>
-      <main>
-        <Routes>
-          <Route path="/" element={<HomePage/>}/>
-          <Route path="/login" element={!!authUser ? <Navigate to="/" /> : <LoginPage />}/>
-          <Route path="/register" element={!!authUser ? <Navigate to="/" /> : <RegisterPage />}/>
-          <Route path="/thread/:id" element={<ThreadPage/>}/>
-          <Route path="/thread/create" element={<ThreadAddPage/>}/>
-          <Route path="/leaderboard" element={<LeaderboardPage/>}/>
-        </Routes>
-      </main>
-    </div>
+      <div className="app-container">
+        <Toaster />
+        <header>
+          <h1>Forum App</h1>
+          <Navigation logout={onLogout} authUser={authUser} />
+        </header>
+        <LoadingBar style={{ backgroundColor: "#27374D" }} />
+        <main>
+          {routes}
+        </main>
+      </div>
   );
 }
 
