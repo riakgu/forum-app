c9b7b5586806d0cd7ff5c8f548e3d7256fe0988d
riakgu <rizkyakbargusnaen@gmail.com>
2024-05-11 06:54:14 +0700

    Appy prettier formatting

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './index.css';
-import store from './states/store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import store from "./states/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <BrowserRouter>
