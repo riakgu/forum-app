a3692f44a4447cb81a1270d3a03b45dc86fd9445
riakgu <rizkyakbargusnaen@gmail.com>
2024-05-12 08:34:41 +0700

    Configure eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,39 @@
-import globals from "globals";
-import path from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import pluginJs from "@eslint/js";
+import globals from 'globals';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import pluginCypress from 'eslint-plugin-cypress';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginJs from '@eslint/js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: pluginJs.configs.recommended
+});
 
 export default [
-  { languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  ...compat.extends("airbnb"),
   {
-    plugins: ["cypress"],
-    env: {
-      "cypress/globals": true
-    },
+    languageOptions: {
+      globals: {...globals.browser, ...globals.node}
+    }
+  },
+  ...compat.extends('airbnb'),
+  // pluginReactConfig,
+  {
+    // plugins: ['cypress'],
+    // env: {
+    //   'cypress/globals': true
+    // },
+    // extends: ['plugin:cypress/recommended'],
     rules: {
       'import/no-named-as-default': 'off',
       'import/no-named-as-default-member': 'off',
+      'react/jsx-props-no-spreading': 'off',
+      'react/jsx-filename-extension': 'off',
+      'import/no-extraneous-dependencies': 'off',
     }
   }
 ];
